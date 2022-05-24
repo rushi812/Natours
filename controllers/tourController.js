6a6261e0e49583552c625bb01c225def9e667f53
Rushiraj Brahmbhatt <rushiraj@fancraze.com>
2022-05-24 20:15:07 +0530

    Filtering, Sorting, Limiting, Pagination, Aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -4,23 +4,68 @@ const Tour = require('../models/tourModel');
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
 
+exports.alisaTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+    // 1A) Filtering
+    // duration=5&difficulty=easy
     const queryObj = { ...req.query };
     const excudedFields = ['page', 'sort', 'limit', 'fields'];
     excudedFields.forEach((el) => delete queryObj[el]);
 
-    const query = Tour.find(queryObj);
+    // 1B) Advanced Filtering
+    // duration[gte]=5&difficulty=easy&price[lt]=1200
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    // sort=price,ratingsAverage (Ascending) for Descinding add "-" in front of the sorting field i.e sort=-price
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // 3) Field Limiting
+    // fields=name,duration,price
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v'); // Excluding the __v field
+    }
+
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist!');
+    }
+
+    // EXECUTE QUERY
+    const tours = await query;
 
     // const tours = await Tour.find()
     //   .where('duraction')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    // EXECUTE QUERY
-    const tours = await query;
 
     // SEND RESPONSE
     res.status(200).json({
@@ -31,7 +76,7 @@ exports.getAllTours = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: 'FAILED',
-      message: err,
+      message: err.message,
     });
   }
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,8 +3,16 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
-const { getAllTours, createTour, getTour, updateTour, deleteTour } =
-  tourController;
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  alisaTopTours,
+} = tourController;
+
+router.route('/top-5-cheap').get(alisaTopTours, getAllTours);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
